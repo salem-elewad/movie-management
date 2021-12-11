26ff96f87a39b45021fae49e698a85d228c46dbd
Mamadou Aly Sy <sy.aly.mamadou@gmail.com>
2021-12-11 11:32:51 +0000

    Ajout de la vue show

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -45,9 +45,9 @@ class FilmController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Film $film)
     {
-        //
+        return view('show', compact('film'));
     }
 
     /**
